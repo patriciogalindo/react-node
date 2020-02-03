4000cd7a1448c1095d01aaa07c421d62d97ae87d
patricio galindo <patriciogalindo@gmail.com>
2020-02-03 18:57:16 +0200

    change according documentation1

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ require('./routes/authRoutes')(app);
 require('./routes/billingRoutes')(app);
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, "client/build"));
+  app.use(express.static(path.join(__dirname, "client/build")));
   app.get('*', (req, res) => {
     res.sendFile(path.join( __dirname, "client", "build", "index.html"));
   });
