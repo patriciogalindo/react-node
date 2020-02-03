9ac2680f678ce3c1daa9c40e393a8c1394a756ad
patricio galindo <patriciogalindo@gmail.com>
2020-02-03 16:01:49 +0200

    change1

--- services/passport.js
+++ services/passport.js
@@ -19,7 +19,7 @@ passport.use(
   new GoogleStrategy({
     clientID: keys.googleClientID,
     clientSecret: keys.googleClientSecret,
-    callbackURL: 'http://localhost:3000/auth/google/callback',
+    callbackURL: '/auth/google/callback',
     proxy: true
   },
   async (accessToken, refreshToken, profile, done) => {
