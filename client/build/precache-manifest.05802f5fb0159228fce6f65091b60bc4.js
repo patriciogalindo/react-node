209b8efc1ac5ea4464b2aa4c1f6715d26d1c873c
patricio galindo <patriciogalindo@gmail.com>
2020-02-03 19:09:49 +0200

    took out from gitignore

---
+++ client/build/precache-manifest.05802f5fb0159228fce6f65091b60bc4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bbe14c5baac232e29f1f35394afe2f9c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e2449a99da9e5b54d840",
+    "url": "/static/css/2.d88a60dc.chunk.css"
+  },
+  {
+    "revision": "e2449a99da9e5b54d840",
+    "url": "/static/js/2.8cdbb010.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/2.8cdbb010.chunk.js.LICENSE"
+  },
+  {
+    "revision": "4971dfe3213776c6f3e4",
+    "url": "/static/js/main.5426a65b.chunk.js"
+  },
+  {
+    "revision": "5d93c6b2d15332364552",
+    "url": "/static/js/runtime-main.cea588d5.js"
+  }
+]);
