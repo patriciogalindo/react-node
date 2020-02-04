49bed577443386d064f2ae283e6b918ae7f11119
patricio galindo <patriciogalindo@gmail.com>
2020-02-04 19:24:30 +0200

    surveys

--- index.js
+++ index.js
@@ -4,7 +4,9 @@ const mongoose = require('mongoose');
 const cookieSession = require('cookie-session');
 const passport = require('passport');
 const bodyParser = require('body-parser');
-require('./models/user')
+require('./models/user');
+require('./models/survey');
+
 require('./services/passport');
 
 mongoose.connect(keys.mongoURI);
@@ -24,6 +26,7 @@ app.use(passport.session());
 
 require('./routes/authRoutes')(app);
 require('./routes/billingRoutes')(app);
+require('./routes/surveyRoutes')(app);
 
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static("client/build"));
